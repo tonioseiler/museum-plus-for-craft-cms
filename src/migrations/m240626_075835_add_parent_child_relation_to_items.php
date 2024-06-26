0f3412dd7ac5c4d42dc0af5212c508804286b6a0
Tonio Seiler <tonio@furbo.ch>
2024-06-26 15:29:58 +0200

    das

--- src/migrations/m240626_075835_add_parent_child_relation_to_items.php
+++ src/migrations/m240626_075835_add_parent_child_relation_to_items.php
@@ -15,7 +15,7 @@ class m240626_075835_add_parent_child_relation_to_items extends Migration
      */
     public function safeUp(): bool
     {
-        if (!$this->db->columnExists('{{%museumplus_items}}', 'parentId')) {y
+        if (!$this->db->columnExists('{{%museumplus_items}}', 'parentId')) {
             $this->addColumn('{{%museumplus_items}}', 'parentId', $this->integer()->notNull()->defaultValue(0));
         }
         return true;
