b278f64c107c5b8d616a35ae3c918d3bf20cec56
paolo <paolo@furbo.ch>
2025-02-27 12:46:35 +0100

    debug element has url only on first site

--- src/jobs/UpdateItemJob.php
+++ src/jobs/UpdateItemJob.php
@@ -438,7 +438,7 @@ class UpdateItemJob extends BaseJob
 
         }
 
-        $inventoryNumber = "mz-stupi-inv-123";//$item->getDataAttribute('ObjObjectNumberVrt');
+        $inventoryNumber = "mz-stupi-inv-890";//$item->getDataAttribute('ObjObjectNumberVrt');
         if (empty($inventoryNumber))
             $inventoryNumber = $item->getDataAttribute('ObjObjectNumberTxt');
 
