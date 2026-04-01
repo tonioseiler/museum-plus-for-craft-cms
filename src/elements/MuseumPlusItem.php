d40721e8f7bdc7f10c41e9c27c916fa0696692f4
Tonio Seiler <tonio@furbo.ch>
2026-04-01 21:33:42 +0200

    Perf. improvements

--- src/elements/MuseumPlusItem.php
+++ src/elements/MuseumPlusItem.php
@@ -501,21 +501,21 @@ class MuseumPlusItem  extends Element
             $vocabularyTypes = self::getVocabularyTypes();
             foreach($vocabularyTypes as $vocabularyType) {
                 $ves = $this->getVocabularyEntriesByType($vocabularyType)->select('id', 'data')->all();
-                // foreach($ves as $ve) {
-                //     $v = $ve->getDataAttribute('content');
-                //     if (!empty($v)) {
-                //         $tmp[] = $v;
-                //     }
-                // }
+                foreach($ves as $ve) {
+                    $v = $ve->getDataAttribute('content');
+                    if (!empty($v)) {
+                        $tmp[] = $v;
+                    }
+                }
             }
 
-            // $filtered = array_filter($tmp, function ($v) {
-            //     return !is_numeric($v) && strlen($v) > 2 && $v != "true" && $v != "false";
-            // });
-            // $unique = array_unique($filtered);
-            // sort($unique);
-            // $ret = implode(' ', $unique);
-            // return $ret;
+            $filtered = array_filter($tmp, function ($v) {
+                return !is_numeric($v) && strlen($v) > 2 && $v != "true" && $v != "false";
+            });
+            $unique = array_unique($filtered);
+            sort($unique);
+            $ret = implode(' ', $unique);
+            return $ret;
         } else {
             return parent::getSearchKeywords($attribute);
         }
